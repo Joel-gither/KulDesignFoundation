17759eb9194e79c22ef65a60374eb009243de31f
icongraphics45@gmail.com <IconGraphics>
2025-04-25 15:52:09 +0300

    db trial

--- connect.php
+++ connect.php
@@ -1,17 +1,18 @@
 <?php
-$fullName = $_POST['fulltname'];
+$fullName = $_POST['fullName'];
 $email = $_POST['email'];
+$subject = $_POST['subject'];
 $message = $_POST['message'];
 
 //connection
 
-$conn = new mysqli('localhost', 'root','','feedback');
+$conn = new mysqli('localhost', 'root','','kdyfo');
 if($conn->connect_error){
-    die('connection Failed : .$conn->connect_error');
+    die('Connection Failed : '.$conn->connect_error);
 
 }else{
-    $stmt = $conn->prepare("insert into registration(fullName,email,message) values(?,?,?,)");
-    $stmt->bind_param("sss" , $fullName, $email, $message);
+    $stmt = $conn->prepare("insert into feedback(fullName,email,subject,message) values(?,?,?,?)");
+    $stmt->bind_param("ssss" , $fullName, $email, $message);
     $stmt->execute();
     echo "Message Received";
     $stmt->close();
